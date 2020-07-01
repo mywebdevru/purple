76b3aaab10fabdc50ca0626721c95331344ca002
Russkazkin <ruslan@skazkin.su>
2020-07-01 07:45:31 +0200

    Update dev server

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\games\os\ospanel\domains\purple\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\games\os\ospanel\domains\purple\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/purple/dev/purple/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/purple/dev/purple/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
