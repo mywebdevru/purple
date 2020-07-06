1859dc56ab2ac51d3b20dd4342ef01a1a23bcea9
Russkazkin <ruslan@skazkin.su>
2020-07-06 07:06:06 +0300

    Add minor fixes

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
