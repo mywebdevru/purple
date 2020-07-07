6c9467eded3719cdd072050509a9220811bc2c36
Russkazkin <ruslan@skazkin.su>
2020-07-07 08:18:22 +0300

    Add user dd in show method

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -45,9 +45,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        dd($user);
     }
 
     /**
