fc0df2afc08505dc2b5d16f24e4fd44fe570f4d6
Russkazkin <ruslan@skazkin.su>
2020-07-09 16:12:56 +0300

    Fix user email validation

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -26,7 +26,10 @@ class UpdateProfileRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email' => 'required',
+            'email' => [
+                'required',
+                Rule::unique('users')->ignore($this->user),
+            ],
             /*|unique:users,email,' . $this->user,*/
             'surname'  => 'required',
             'avatar' => 'image|dimensions:min_width=100,min_height=100,max_width=800,max_height=800',
