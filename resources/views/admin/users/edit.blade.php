68ccf7bb4e209c21a381b758d149bd28d9316fd3
Russkazkin <ruslan@skazkin.su>
2020-07-09 10:43:44 +0300

    Start to create user edit form

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -12,7 +12,19 @@
             Редактирование профиля пользователя
         </div>
         <div class="card-body">
-
+            @include('admin.partials.errors')
+            <form action="{{ route('admin.users.update', $user->id) }}">
+                @method('PATCH')
+                @csrf
+                <div class="form-group">
+                    <label for="name">Имя</label>
+                    <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
+                </div>
+                <div class="form-group">
+                    <label for="surname">Фамилия</label>
+                    <input type="text" name="surname" id="surname" class="form-control" value="{{ $user->surname }}">
+                </div>
+            </form>
         </div>
     </div>
 @endsection
