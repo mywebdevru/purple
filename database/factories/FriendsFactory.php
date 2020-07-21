fcb4f2e813993742db8d588878c5e6813935ce28
x3mart <45395171+x3mart@users.noreply.github.com>
2020-07-21 22:16:21 +0300

    Фабрика FriendsFactory создает Друзей парой и оформляет подписку друг на друга. Способ реализации загадочен, но работает.

--- database/factories/FriendsFactory.php
+++ database/factories/FriendsFactory.php
@@ -4,12 +4,27 @@
 
 use App\Model;
 use App\Friend;
+use App\User;
 use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
+
 $factory->define(Friend::class, function (Faker $faker) {
-    return [
-        'user_id' => rand(1, App\User::all()->count()),
-        'friend_id' => rand(1, App\User::all()->count()),
+    $user_id = rand(1, App\User::all()->count());
+    $friend_id = rand(1, App\User::all()->count());
+    Friend::create([
+        'user_id' => $user_id,
+        'friend_id' => $friend_id,
+    ]);
+    Friend::create([
+        'user_id' => $friend_id,
+        'friend_id' => $user_id,
+    ]);
+    User::find($user_id)->subscribesToUsers()->attach($friend_id);
+    User::find($friend_id)->subscribesToUsers()->attach($user_id);
+    return
+    [
+            'user_id' => App\User::all()->count(),
+            'friend_id' => App\User::all()->count(),
     ];
 });
