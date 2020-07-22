cb35ca9ee2a0ac326cc12bc281754bbaece6cf18
Russkazkin <ruslan@skazkin.su>
2020-07-22 07:30:03 +0300

    Generate SummernoteController

---
+++ app/Http/Controllers/Shared/SummernoteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Shared;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SummernoteController extends Controller
+{
+    //
+}
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -48,7 +48,7 @@
                 disableDragAndDrop: false,
                 callbacks: {
                     onImageUpload: function (files) {
-                        console.log(files);
+                        uploadFile(files);
                     },
 
                     onMediaDelete: function ($target, editor, $editable) {
@@ -59,6 +59,56 @@
                 }
             }
             $('#text').summernote(config);
+
+            function uploadFile(files) {
+                const data = new FormData();
+
+                for (let i = 0; i < files.length; i++) {
+                    data.append("files[]", files[i]);
+                    data.append('username', 'Chris');
+                }
+                console.log(data);
+
+                $.ajax({
+                    data: data,
+                    type: "POST",
+                    url: "/ajax/uploader/upload",
+                    cache: false,
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    contentType: false,
+                    processData: false,
+                    success: function (images) {
+                        //console.log(images);
+
+                        // If not errors.
+                        if (typeof images['error'] == 'undefined') {
+
+                            // Get all images and insert to editor.
+                            for (var i = 0; i < images['url'].length; i++) {
+
+                                editor.summernote('insertImage', images['url'][i], function ($image) {
+                                    //$image.css('width', $image.width() / 3);
+                                    //$image.attr('data-filename', 'retriever')
+                                });
+                            }
+                        }
+                        else {
+                            // Get user's browser language.
+                            var userLang = navigator.language || navigator.userLanguage;
+
+                            if (userLang == 'ru-RU') {
+                                var error = 'Ошибка, не могу загрузить файл! Пожалуйста, проверьте файл или ссылку. ' +
+                                    'Изображение должно быть не менее 500px!';
+                            }
+                            else {
+                                var error = 'Error, can\'t upload file! Please check file or URL. Image should be more then 500px!';
+                            }
+
+                            alert(error);
+                        }
+                    }
+                });
+            }
         });
     </script>
 @endsection
