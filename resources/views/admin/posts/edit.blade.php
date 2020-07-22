706ffe5ab656b371a6ff86e527a126aa94596302
Russkazkin <ruslan@skazkin.su>
2020-07-22 09:06:07 +0300

    Rewrite image upload with async/await axios

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -61,50 +61,26 @@
                 }
             editor.summernote(config);
 
-            function uploadFile(files) {
+            async function uploadFile(files) {
                 const data = new FormData();
 
                 for (let i = 0; i < files.length; i++) {
                     data.append("files[]", files[i]);
                 }
-
-                $.ajax({
-                    data: data,
-                    type: "POST",
-                    url: "{{ route('summernote.upload') }}",
-                    cache: false,
-                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                    contentType: false,
-                    processData: false,
-                    success: function (images) {
-                        console.log(images);
-
-                        // If not errors.
-                        if (typeof images['error'] == 'undefined') {
-
-                            // Get all images and insert to editor.
-                            for (let i = 0; i < images.length; i++) {
-
-                                editor.summernote('insertImage', '/storage/' + images[i], function ($image) {
-                                    //$image.css('width', $image.width() / 3);
-                                    //$image.attr('data-filename', 'retriever')
-                                });
-                            }
-                        } else {
-                            // Get user's browser language.
-                            var userLang = navigator.language || navigator.userLanguage;
-
-                            if (userLang == 'ru-RU') {
-                                var error = 'Ошибка, не могу загрузить файл! Пожалуйста, проверьте файл или ссылку. ' +
-                                    'Изображение должно быть не менее 500px!';
-                            } else {
-                                var error = 'Error, can\'t upload file! Please check file or URL. Image should be more then 500px!';
-                            }
-
-                            alert(error);
-                        }
+                try {
+                    const images = (await axios({
+                        data,
+                        method: 'post',
+                        url: "{{ route('summernote.upload') }}",
+                    })).data;
+                    for (let i = 0; i < images.length; i++) {
+                        editor.summernote('insertImage', '/storage/' + images[i], function ($image) {
+                            $image.css('width', '100%');
+                        });
                     }
-                });
+                } catch (e) {
+                    console.log(e);
+                }
             }
         });
     </script>
