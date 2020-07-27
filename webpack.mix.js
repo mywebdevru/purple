0609fce05ec7890f8fdfee605c428ad48c03c86e
x3mart <45395171+x3mart@users.noreply.github.com>
2020-07-27 14:15:37 +0300

    Сборка скриптов для нового шаблона в один main.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/admin.js', 'public/js')
+    .scripts(['resources/js/libs/fm.revealator.jquery.js',
+            'resources/js/libs/jquery.appear.js',
+            'resources/js/libs/jquery.magnific-popup.js',
+            'resources/js/libs/material.min.js',
+            'resources/js/libs/perfect-scrollbar.js',
+            'resources/js/main.js',
+            'resources/js/libs-init/libs-init.js'] ,'public/js/main.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/admin.sass', 'public/css');
