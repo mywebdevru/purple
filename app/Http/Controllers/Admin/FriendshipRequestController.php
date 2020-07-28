815b8f4a1a3654af15f106fc58270eb614490fac
Russkazkin <ruslan@skazkin.su>
2020-07-28 08:25:09 +0300

    Generate FriendshipRequestController

---
+++ app/Http/Controllers/Admin/FriendshipRequestController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\FriendshipRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FriendshipRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FriendshipRequest  $friendshipRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FriendshipRequest $friendshipRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FriendshipRequest  $friendshipRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FriendshipRequest $friendshipRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FriendshipRequest  $friendshipRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FriendshipRequest $friendshipRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FriendshipRequest  $friendshipRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FriendshipRequest $friendshipRequest)
+    {
+        //
+    }
+}
