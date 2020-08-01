3e2388690152132aab8500962be3037038a77eb9
Russkazkin <ruslan@skazkin.su>
2020-08-01 09:02:10 +0300

    Add summernote db image save

--- app/Http/Controllers/Shared/SummernoteController.php
+++ app/Http/Controllers/Shared/SummernoteController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers\Shared;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Summernote\SummernoteDeleteRequest;
 use App\Http\Requests\Summernote\SummernoteUploadRequest;
+use App\User;
 use Storage;
 
 class SummernoteController extends Controller
 {
     public function upload(SummernoteUploadRequest $request)
     {
-        return $request;
         $images = [];
         foreach ($request['files'] as $image) {
             $img = $image->store('summernote');
+            $user = User::find(auth()->user()->id);
+            $user->images()->create(['image' => $img]);
             $images[] = $img;
             $pic = \Image::make('storage/' . $img);
             $width = $pic->width();
