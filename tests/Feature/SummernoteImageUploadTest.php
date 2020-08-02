34f8a00debc26c7e5cac9b09ff3138f9d83b2d0b
Russkazkin <ruslan@skazkin.su>
2020-08-02 09:37:13 +0300

    Add post creation for Summernote upload test

--- tests/Feature/SummernoteImageUploadTest.php
+++ tests/Feature/SummernoteImageUploadTest.php
@@ -3,18 +3,20 @@
 namespace Tests\Feature;
 
 
+use App\Post;
 use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class SummernoteImageUploadTest extends TestCase
 {
-    public function testSummernoteUploadFile()
+    public function SummernoteUploadFile()
     {
         $response = $this->call('post', route('summernote.upload'),
             [
                 'files' => [
                     UploadedFile::fake()->image('test.jpg'),
                 ],
+                'post' => factory(Post::class)->create(),
             ],
             [
                 'Content-Type' => 'multipart/form-data',
