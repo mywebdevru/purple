05a295f505ed5b966631b7bd13a07c55ea71e127
Russkazkin <ruslan@skazkin.su>
2020-08-02 10:47:06 +0300

    Add minor fixes

--- tests/Feature/ProfilePageTest.php
+++ tests/Feature/ProfilePageTest.php
@@ -34,13 +34,12 @@ class ProfilePageTest extends TestCase
         $this->actingAs($user);
 
         $response1 = $this->get(route('user.show', 1));
-        $response3 = $this->get(route('user.show', 20));
-        $response4 = $this->get(route('user.show', $user->id));
+        $response2 = $this->get(route('user.show', 20));
+
 
         $response1->assertStatus(200);
-        $response3->assertStatus(200);
-        $response3->assertStatus(200);
-        $response4->assertStatus(200);
+        $response2->assertStatus(200);
+
     }
 
     public function testUserCanViewOwnProfile()
