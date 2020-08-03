838306743baa7e60ef5cfa4be34b69765f7950e7
x3mart <45395171+x3mart@users.noreply.github.com>
2020-08-03 16:32:06 +0300

    Блок автомобилей пользователя

--- resources/views/user/prof.blade.php
+++ resources/views/user/prof.blade.php
@@ -1406,7 +1406,41 @@
 						</li>
 					</ul>
 				</div>
-			</div>
+            </div>
+
+			<!-- Автомобили -->
+            <div class="ui-block revealator-slideup revealator-once">
+                @if ($data->usersVehicles->isEmpty())
+                    <div class="ui-block-title">
+                        <h6 class="title">Я хожу пешком</h6>
+                    </div>
+                @else
+                    <div class="ui-block-title">
+                        <h6 class="title">Я катаюсь на:</h6>
+                    </div>
+                @endif
+
+				<ul class="widget w-friend-pages-added notification-list friend-requests js-zoom-gallery">
+                    @forelse ($data->usersVehicles as $item)
+                        <li class="inline-items">
+                            <div class="author-thumb">
+                                <img src="{{ $item->avatar }}" alt="author" class="avatar">
+                            </div>
+                            <div class="notification-event">
+                            <a href="{{ $item->avatar }}" class="h6 notification-friend">{{ $item->type }}</a>
+                                <span class="chat-message-item">{{ $item->full_vehicle_name }}</span>
+                            </div>
+                            <span class="notification-icon" data-toggle="tooltip" data-placement="top" data-original-title="ADD TO YOUR FAVS">
+                                <a href="#">
+                                    <svg class="olymp-star-icon"><use xlink:href="{{ asset('svg-icons/sprites/icons.svg#olymp-star-icon') }}"></use></svg>
+                                </a>
+                            </span>
+                        </li>
+                    @empty
+                    @endforelse
+				</ul>
+            </div>
+            <!-- .. окончание Авто -->
 
 			<div class="ui-block revealator-slideup revealator-once">
 				<div class="ui-block-content">
