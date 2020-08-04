f6e85fb3969b4dd42748d7f3b01ec7ab7cc031a7
x3mart <45395171+x3mart@users.noreply.github.com>
2020-08-04 14:11:30 +0300

    Механизм передачи данных для хедера в нужшые шаблоны

---
+++ app/Http/View/Composers/ProfileComposer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\User;
+use Illuminate\View\View;
+
+class ProfileComposer
+{
+    /**
+     * The user repository implementation.
+     *
+     * @var UserRepository
+     */
+    protected $users;
+
+    /**
+     * Create a new profile composer.
+     *
+     * @param  UserRepository  $users
+     * @return void
+     */
+    public function __construct(User $users)
+    {
+        // Dependencies automatically resolved by service container...
+        $this->users = $users;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        if (!!auth()->user()){
+            $user = $this->users->find(auth()->user()->id);
+            $user->loadCount('requestedFriendships');
+            $user->load('requestedFriendships.user', 'friendshipRequests');
+            $view->with('user', $user);
+        }
+
+    }
+}
