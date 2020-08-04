7e1c9c25a682a936a8b8b9700a30169c41ac41bf
x3mart <45395171+x3mart@users.noreply.github.com>
2020-08-04 14:41:06 +0300

    Вернул роуту утраченное с моей помощью имя

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group([
     'namespace' => 'User',
