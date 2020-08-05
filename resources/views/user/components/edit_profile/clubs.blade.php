67fdb52d196b0223b1d8214665de643366136287
x3mart <45395171+x3mart@users.noreply.github.com>
2020-08-05 16:36:18 +0300

    Вариант дизайна страницы Клубы от Алекса

--- resources/views/user/components/edit_profile/clubs.blade.php
+++ resources/views/user/components/edit_profile/clubs.blade.php
@@ -9,10 +9,10 @@
     @component('user.components.wallpaper_block.main', ['data' => $user, 'user' => $user])@endcomponent
 
     <a class="back-to-top" href="#">
-        <img src="../../svg-icons/back-to-top.svg" alt="arrow" class="back-icon">
+        <img src="../../../svg-icons/back-to-top.svg" alt="arrow" class="back-icon">
     </a>
 
     <div class="container pl-4">
-        Ваш код здесь. Но стили этого дива можно поменять.
+        
     </div>
 @endsection
