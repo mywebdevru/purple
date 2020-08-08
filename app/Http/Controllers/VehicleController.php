2b6cbbb53258f5a50ec84f988f440a6fde443ab1
Russkazkin <ruslan@skazkin.su>
2020-08-08 08:08:19 +0300

    Generate VehicleController

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserVehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserVehicle  $userVehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserVehicle $userVehicle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserVehicle  $userVehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserVehicle $userVehicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserVehicle  $userVehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserVehicle $userVehicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserVehicle  $userVehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserVehicle $userVehicle)
+    {
+        //
+    }
+}
