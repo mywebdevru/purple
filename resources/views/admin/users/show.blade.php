7d855132fc95d530f2e3722d8ccabc35d7d1a5fc
Russkazkin <ruslan@skazkin.su>
2020-08-08 09:32:30 +0300

    Complete modal to add vechicle

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -237,13 +237,93 @@
                         @endforelse
                         </tbody>
                     </table>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addVehicleModal">
+                        Добавить транспортное средство
+                    </button>
                 </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="addVehicleModal" tabindex="-1" aria-labelledby="addVehicleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Добавление транспортного средства</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="type">Тип транспортного средства</label>
+                            <select class="form-control" id="type" name="type">
+                                <option disabled selected>Выберите тип транспортного средства</option>
+                                <option value="1">Легковой</option>
+                                <option value="2">Грузовой или автобус</option>
+                                <option value="3">Коммерческий</option>
+                                <option value="4">Мотоцикл/квадрацикл</option>
+                            </select>
+                        </div>
+                        <div class="form-group" id="brands-group" style="display: none">
+                            <label for="brand">Производитель</label>
+                            <input list="brand-list" id="brand" name="brand" class="form-control" placeholder="Начните вводить производителя...">
+                            <datalist id="brand-list"></datalist>
+                        </div>
+                        <div class="form-group" id="models-group" style="display: none">
+                            <label for="model">Модель</label>
+                            <input list="model-list" id="model" name="model" class="form-control" placeholder="Начните вводить марку...">
+                            <datalist id="model-list"></datalist>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                        <button type="submit" class="btn btn-primary">Добавить</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 @endsection
 
 @section('scripts')
+<script>
+$(document).ready(function () {
+    $('#type').on('change', function () {
+        const type = $(this).val();
+        (async function getCategory() {
+            try {
+                const brands = (await axios.get('https://auto-db.mywebdev.ru/api/category/' + type)).data.data;
+                console.log(brands);
+                brands.forEach(function (brand) {
+                    $("#brand-list").append('<option value="' + brand.name + '" data-brand="' + brand.id + '">');
+                });
+                $('#brands-group').fadeIn();
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+    });
+    $('#brand').on('change', function () {
+        const brand = $(this).val(),
+            brandID = $(`#brand-list option[value=${brand}]`).data('brand');
+        (async function getBrand() {
+            try {
+                const models = (await axios.get('https://auto-db.mywebdev.ru/api/manufacturer/' + brandID)).data.data;
+                console.log(models);
+                models.forEach(function (model) {
+                    $("#model-list").append('<option value="' + model.name + '" data-brand="' + model.id + '">');
+                });
+                $('#models-group').fadeIn();
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+        console.log(brandID);
+    });
+});
+</script>
 @endsection
 @section('css')
     <style>
