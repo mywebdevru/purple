8ba7ad1ea71ad63900c907afcd855cc164449284
Russkazkin <ruslan@skazkin.su>
2020-08-09 15:11:01 +0300

    Bind user id into component

--- resources/js/profile/ProfileEditForm.vue
+++ resources/js/profile/ProfileEditForm.vue
@@ -56,6 +56,9 @@
 <script>
 export default {
     name: "ProfileEditForm",
+    props: {
+        id: Number,
+    },
     data() {
         return {
             name: null,
--- resources/views/user/components/edit_profile/personal.blade.php
+++ resources/views/user/components/edit_profile/personal.blade.php
@@ -28,7 +28,7 @@ $data = $profile;
                     </h3>
                 </div>
                 <div class="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12" id="edit-profile">
-                    <profile-edit-form></profile-edit-form>
+                    <profile-edit-form :id="{{ $profile->id }}"></profile-edit-form>
                 </div>
 
                 <div class="col col-xl-3 order-xl-1 col-lg-6 order-lg-2 col-md-6 col-sm-6 col-12">
