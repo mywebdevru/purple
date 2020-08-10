b52cb45bbddc53bdc65d069d106685a39ac7c3dc
x3mart <45395171+x3mart@users.noreply.github.com>
2020-08-10 15:55:34 +0300

    скрыл иконку загрузки фото и кнопку отмены

--- resources/views/user/components/feed/write_comment.blade.php
+++ resources/views/user/components/feed/write_comment.blade.php
@@ -8,15 +8,15 @@
         <img src="{{ asset($comment_author->avatar) }}" alt="author">
         <div class="form-group with-icon-right">
             <textarea class="form-control" placeholder="" name="text"></textarea>
-            <div class="add-options-message">
+            {{-- <div class="add-options-message">
                 <a href="#" class="options-message" data-toggle="modal" data-target="#update-header-photo">
                     <svg class="olymp-camera-icon">
                         <use xlink:href="{{ asset('svg-icons/sprites/icons.svg#olymp-camera-icon') }}"></use>
                     </svg>
                 </a>
-            </div>
+            </div> --}}
         </div>
     </div>
     <button type="submit" class="btn btn-md-2 btn-primary">Запостить комментарий</button>
-    <button class="btn btn-md-2 btn-border-think c-grey btn-transparent custom-color">Отмена</button>
+    {{-- <button class="btn btn-md-2 btn-border-think c-grey btn-transparent custom-color" onclick="event.preventDefault(); $('#write_comment_'{{ $feed }}).slideUp();">Отмена</button> --}}
 </form>
