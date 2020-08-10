564847625ac52041c0945614d59bf6cc58a61c0e
Russkazkin <ruslan@skazkin.su>
2020-08-10 06:15:47 +0300

    Add submit form method

--- resources/js/components/profile/ProfileEditForm.vue
+++ resources/js/components/profile/ProfileEditForm.vue
@@ -2,49 +2,52 @@
     <div class="ui-block">
         <article class="hentry post video">
             <div class="col-sm-7">
-                <div class="form-group">
-                    <label for="name">Изменить имя</label>
-                    <input type="text" class="form-control form_edit_profile_field" id="name" name="name" v-model="profile.name">
-                    <small id="InputName1Help" class="form-text text-muted">Имя, которое видят все.</small>
-                </div>
+                <form @submit.prevent="save">
+                    <div class="form-group">
+                        <label for="name">Изменить имя</label>
+                        <input type="text" class="form-control form_edit_profile_field" id="name" name="name" v-model="profile.name">
+                        <small id="InputName1Help" class="form-text text-muted">Имя, которое видят все.</small>
+                    </div>
 
-                <div class="form-group">
-                    <label for="surname">Изменить фамилию</label>
-                    <input type="text" class="form-control form_edit_profile_field" id="surname" name="surname" v-model="profile.surname">
-                    <small id="InputName2Help" class="form-text text-muted">Фамилия, которую видят все.</small>
-                </div>
+                    <div class="form-group">
+                        <label for="surname">Изменить фамилию</label>
+                        <input type="text" class="form-control form_edit_profile_field" id="surname" name="surname" v-model="profile.surname">
+                        <small id="InputName2Help" class="form-text text-muted">Фамилия, которую видят все.</small>
+                    </div>
 
-                <div class="form-group">
-                    <label for="email">Сменить Email</label>
-                    <input type="email" class="form-control form_edit_profile_field" id="email" name="email" v-model="profile.email">
-                    <small id="emailHelp" class="form-text text-muted"></small>
-                </div>
+                    <div class="form-group">
+                        <label for="email">Сменить Email</label>
+                        <input type="email" class="form-control form_edit_profile_field" id="email" name="email" v-model="profile.email">
+                        <small id="emailHelp" class="form-text text-muted"></small>
+                    </div>
 
-                <div class="form-group">
-                    <label for="gender">Выберите пол...</label>
-                    <select class="form-control form_edit_profile_field" id="gender" name="gender" v-model="profile.gender">
-                        <option :selected="profile.gender === 'Мужчина'">Мужчина</option>
-                        <option :selected="profile.gender === 'Женщина'">Женщина</option>
-                        <option :selected="profile.gender === 'В смятении'">В смятении</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="birth_date">Ваша дата рождения</label>
-                    <input type="date" name="birth_date" class="form-control form_edit_profile_field" id="birth_date" v-model="profile.birth_date">
-                </div>
+                    <div class="form-group">
+                        <label for="gender">Выберите пол...</label>
+                        <select class="form-control form_edit_profile_field" id="gender" name="gender" v-model="profile.gender">
+                            <option :selected="profile.gender === 'Мужчина'">Мужчина</option>
+                            <option :selected="profile.gender === 'Женщина'">Женщина</option>
+                            <option :selected="profile.gender === 'В смятении'">В смятении</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="birth_date">Ваша дата рождения</label>
+                        <input type="date" name="birth_date" class="form-control form_edit_profile_field" id="birth_date" v-model="profile.birth_date">
+                    </div>
 
-                <div class="form-group">
-                    <label for="city">Выберите город</label>
-                    <input type="text" class="form-control form_edit_profile_field" id="city" name="city" v-model="profile.city">
-                </div>
-                <div class="form-group">
-                    <label for="country">Выберите страну</label>
-                    <input type="text" class="form-control form_edit_profile_field" id="country" name="country" v-model="profile.country">
-                </div>
-                <div class="form-group">
-                    <label for="creed">Девиз</label>
-                    <input type="text" class="form-control form_edit_profile_field" id="creed" name="creed" v-model="profile.creed">
-                </div>
+                    <div class="form-group">
+                        <label for="city">Выберите город</label>
+                        <input type="text" class="form-control form_edit_profile_field" id="city" name="city" v-model="profile.city">
+                    </div>
+                    <div class="form-group">
+                        <label for="country">Выберите страну</label>
+                        <input type="text" class="form-control form_edit_profile_field" id="country" name="country" v-model="profile.country">
+                    </div>
+                    <div class="form-group">
+                        <label for="creed">Девиз</label>
+                        <input type="text" class="form-control form_edit_profile_field" id="creed" name="creed" v-model="profile.creed">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Сохранить</button>
+                </form>
             </div>
         </article>
     </div>
@@ -72,6 +75,11 @@ export default {
                 creed: null,
             },
             loading: false,
+        }
+    },
+    methods: {
+        async save() {
+            console.log('Save Form Data');
         }
     },
     async created() {
