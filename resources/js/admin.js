ee1e12f6bc1df32035bde45ddf4ff91e673927d2
Russkazkin <ruslan@skazkin.su>
2020-08-23 09:05:17 +0300

    Add vue app init structure

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -3,3 +3,18 @@ require('./bootstrap');
 window.Vue = require('vue');
 window.flatpickr = require('flatpickr');
 window.flatpickrRU = require("flatpickr/dist/l10n/ru.js").default.ru;
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
