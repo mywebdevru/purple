095726f31a2bc464d1342fe5303c32efc8953dd5
Russkazkin <ruslan@skazkin.su>
2020-08-23 09:51:12 +0300

    Install vuex and vue-router

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,23 +1,22 @@
 import Index from "./components/admin/Index";
+import VueRouter from "vue-router";
 
 require('./bootstrap');
 
 window.Vue = require('vue');
+Vue.use(Vuex);
+Vue.use(VueRouter)
+
 window.flatpickr = require('flatpickr');
 window.flatpickrRU = require("flatpickr/dist/l10n/ru.js").default.ru;
 
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component("index", Index)
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app',
+    store,
+    router,
 });
