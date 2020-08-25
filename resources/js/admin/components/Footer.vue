65ad49ca0a45f9b6d9582e21e02e379406006622
Russkazkin <ruslan@skazkin.su>
2020-08-25 10:46:42 +0300

    Add footer component

--- resources/js/admin/components/Footer.vue
+++ resources/js/admin/components/Footer.vue
@@ -1,10 +1,31 @@
 <template>
-<h1>Footer</h1>
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    2016 - 2019 &copy; Adminto theme by <a href="">Coderthemes</a>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-md-right footer-links d-none d-sm-block">
+                        <a href="javascript:void(0);">About Us</a>
+                        <a href="javascript:void(0);">Help</a>
+                        <a href="javascript:void(0);">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
+import { authComputed } from '../state/helpers'
 export default {
-    name: "Footer"
+    name: "Footer",
+    components: {},
+    computed: {
+        ...authComputed,
+    },
+    methods: {},
 }
 </script>
 
