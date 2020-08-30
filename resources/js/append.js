00390e126c110e46257aa7d2cef9cd5847266f80
moskvicheveg <moskvicheveg@gmail.com>
2020-08-30 23:20:38 +0400

    added script for append buttons

---
+++ resources/js/append.js
@@ -0,0 +1,11 @@
+let controlBlockButtonsRight = document.querySelector('.append');
+let controlBlockButtonsLeft = document.querySelector('.control-block-button-left');
+let bgPurple = document.querySelector('#bg-purple');
+
+if (screen.width <= '801') {
+  controlBlockButtonsRight.remove();
+  controlBlockButtonsLeft.append(controlBlockButtonsRight, bgPurple);
+  document.querySelector('#bg-purple').style.marginLeft = '3px';
+}
+
+
