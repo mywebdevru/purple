0ca9b98d6bb1025c8d31c0e243e3bfc95ef5b2e6
Russkazkin <ruslan@skazkin.su>
2020-09-03 16:34:45 +0300

    Add comment seeding

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class PostsSeeder extends Seeder
      */
     public function run()
     {
-        factory(Post::class, 300)->create();
+        factory(Post::class, 300)->create()->each(function (Post $post){
+            $comments = factory(Comment::class, 3)->make(['authorable_id' => $post->postable_id, 'authorable_type' => $post->postable_type]);
+            $post->comments()->saveMany($comments);
+        });
     }
 }
