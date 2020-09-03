11b5aea77bb5feae050617a0a3567c7bb567c17a
Russkazkin <ruslan@skazkin.su>
2020-09-03 17:38:26 +0300

    Fix comments seeder for posts

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class PostsSeeder extends Seeder
@@ -14,7 +15,11 @@ class PostsSeeder extends Seeder
     public function run()
     {
         factory(Post::class, 300)->create()->each(function (Post $post){
-            $comments = factory(Comment::class, 3)->make(['authorable_id' => $post->postable_id, 'authorable_type' => $post->postable_type]);
+            $comments = factory(Comment::class, rand(1, 10))->make();
+            $comments->each(function ($comment) {
+                $comment->authorable_id = rand(1, User::count());
+                $comment->authorable_type = 'App\Models\User';
+            });
             $post->comments()->saveMany($comments);
         });
     }
