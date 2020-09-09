e3d317bc9da8647384a94a2b349dba039c40d15a
Russkazkin <ruslan@skazkin.su>
2020-09-09 11:21:00 +0300

    Remove all preinstall staff form vuex store

--- resources/js/admin/state/store.js
+++ resources/js/admin/state/store.js
@@ -1,12 +1,5 @@
-import modules from './modules'
-
 export default {
     state: {
-        modules,
-        // Enable strict mode in development to get a warning
-        // when mutating state outside of a mutation.
-        // https://vuex.vuejs.org/guide/strict.html
-        strict: process.env.NODE_ENV !== 'production',
-    },
 
+    },
 }
