dc9458a10ac2a13534b38d420832b1b7a6787ba3
Russkazkin <ruslan@skazkin.su>
2020-09-15 15:32:17 +0300

    Add card structure for user chart

--- resources/js/admin/router/views/Dashboard.vue
+++ resources/js/admin/router/views/Dashboard.vue
@@ -1,9 +1,24 @@
 <template>
     <Layout>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-4">
                 <b-card>
-                    This is dashboard page
+                    <div class="dropdown float-right">
+                        <a href="#" class="dropdown-toggle arrow-none card-drop" data-toggle="dropdown" aria-expanded="false">
+                            <i class="mdi mdi-dots-vertical"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item">Action</a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item">Another action</a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item">Something else</a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item">Separated link</a>
+                        </div>
+                    </div>
+                    <h4 class="header-title mt-0 mb-3">Users</h4>
                     <UserChart></UserChart>
                 </b-card>
             </div>
