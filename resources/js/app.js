11dea22989741f8eaf6afa785e54018251eedcbf
x3mart <45395171+x3mart@users.noreply.github.com>
2020-09-16 12:16:34 +0300

    Alpine JS Install

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 require('summernote');
+require('alpinejs')
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
