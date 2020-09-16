d7baeeb2626bc56d02c901a75181e3e96082797b
Russkazkin <ruslan@skazkin.su>
2020-09-16 09:58:09 +0300

    Change main admin panel page title

---
+++ resources/js/admin/components/dashboard/UserBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-card>
+        <div class="dropdown float-right">
+            <a href="#" class="dropdown-toggle arrow-none card-drop" data-toggle="dropdown" aria-expanded="false">
+                <i class="mdi mdi-dots-vertical"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item">Action</a>
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item">Another action</a>
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item">Something else</a>
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item">Separated link</a>
+            </div>
+        </div>
+        <h4 class="header-title mt-0 mb-3">Users</h4>
+        <UserChart />
+    </b-card>
+</template>
+
+<script>
+import UserChart from "./UserChart";
+
+export default {
+    name: "UserBox",
+    components: {UserChart}
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/admin/router/views/Dashboard.vue
+++ resources/js/admin/router/views/Dashboard.vue
@@ -1,34 +1,14 @@
 <template>
     <Layout>
         <div class="row">
-            <div class="col-md-4">
-                <b-card>
-                    <div class="dropdown float-right">
-                        <a href="#" class="dropdown-toggle arrow-none card-drop" data-toggle="dropdown" aria-expanded="false">
-                            <i class="mdi mdi-dots-vertical"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <!-- item-->
-                            <a href="javascript:void(0);" class="dropdown-item">Action</a>
-                            <!-- item-->
-                            <a href="javascript:void(0);" class="dropdown-item">Another action</a>
-                            <!-- item-->
-                            <a href="javascript:void(0);" class="dropdown-item">Something else</a>
-                            <!-- item-->
-                            <a href="javascript:void(0);" class="dropdown-item">Separated link</a>
-                        </div>
-                    </div>
-                    <h4 class="header-title mt-0 mb-3">Users</h4>
-                    <UserChart></UserChart>
-                </b-card>
-            </div>
+            <UserBox class="col-6" />
         </div>
     </Layout>
 </template>
 
 <script>
 import Layout from "../layouts/Main"
-import UserChart from "../../components/dashboard/UserChart";
+import UserBox from "../../components/dashboard/UserBox";
 
 export default {
     name: "Dashboard",
@@ -36,7 +16,7 @@ export default {
         title: 'Dashboard',
         /*meta: [{ name: 'description', content: appConfig.description }],*/
     },
-    components: { UserChart, Layout },
+    components: {UserBox, Layout },
 }
 </script>
 
