7c7145b54ea32d3ef84a5842e91b062ce3c6d96e
Russkazkin <ruslan@skazkin.su>
2020-09-18 08:17:45 +0300

    Add users_cant_fetch_members_data test

--- tests/Feature/Admin/MemberBoxApiTest.php
+++ tests/Feature/Admin/MemberBoxApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,5 +16,13 @@ class MemberBoxApiTest extends TestCase
     {
         $response = $this->get('/api/members-count', ['Accept' => 'application/json']);
         $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function users_cant_fetch_members_data()
+    {
+        $this->actingAs(factory(User::class)->create(), 'api');
+        $response = $this->get('/api/members-count');
+        $response->assertForbidden();
     }
 }
