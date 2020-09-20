55e3e38c7bbe9e68b1ab57695697c0288265854c
Russkazkin <ruslan@skazkin.su>
2020-09-20 12:29:41 +0300

    Add users_cant_fetch_unread_notifications test

--- tests/Feature/Admin/FetchUnreadNotificationsTest.php
+++ tests/Feature/Admin/FetchUnreadNotificationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,5 +16,13 @@ class FetchUnreadNotificationsTest extends TestCase
     {
         $response = $this->get('/api/notifications/unread', ['Accept' => 'application/json']);
         $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function users_cant_fetch_unread_notifications()
+    {
+        $this->actingAs(factory(User::class)->create(), 'api');
+        $response = $this->get('/api/notifications/unread');
+        $response->assertForbidden();
     }
 }
