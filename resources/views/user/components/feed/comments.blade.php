825b9faaffd68c2330e31c827733c1f9015c5430
x3mart <45395171+x3mart@users.noreply.github.com>
2020-09-27 11:29:32 +0300

    unused Views deleted

--- resources/views/user/components/feed/comments.blade.php
+++
@@ -1,5 +0,0 @@
-<ul class="comments-list"  style="display:none;">
-    @foreach ($comments as $item)
-        @component('user.components.feed.comment_body', ['item' => $item])@endcomponent
-    @endforeach
-</ul>
