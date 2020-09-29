d4fc5de2ddcd09944d097a4ddc4c9558451748d7
Russkazkin <ruslan@skazkin.su>
2020-09-29 10:12:34 +0300

    Add api users route

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,11 +13,11 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        return view('admin.users.index')->with('users', User::paginate(12));
+        return response()->json(['users' => '']);
     }
 
     /**
