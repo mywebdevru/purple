c81450bedee8fada9da6c7b10f8512c5c2923b39
Russkazkin <ruslan@skazkin.su>
2020-10-01 09:02:18 +0300

    Add tabel buttons

--- resources/js/admin/components/Table.vue
+++ resources/js/admin/components/Table.vue
@@ -11,8 +11,13 @@
 <script>
 import 'jszip';
 import 'pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import 'datatables.net-bs4';
 import 'datatables.net-buttons-bs4';
+import 'datatables.net-buttons/js/buttons.html5.js';
+import 'datatables.net-buttons/js/buttons.colVis.js';
+import 'datatables.net-buttons/js/buttons.flash.js';
+import 'datatables.net-buttons/js/buttons.print.js';
 import 'datatables.net-keytable-bs4';
 import 'datatables.net-responsive-bs4';
 import 'datatables.net-select-bs4';
@@ -20,8 +25,10 @@ import 'datatables.net-select-bs4';
 export default {
     name: "Table",
     updated() {
-        $("#datatable").DataTable();
-    },
+        pdfMake.vfs = pdfFonts.pdfMake.vfs;
+        const table = $("#datatable").DataTable({lengthChange: !1, buttons: ["copy", "print", "pdfHtml5", "colvis"], keys: !0});
+        table.buttons().container().appendTo("#datatable_wrapper .col-md-6:eq(0)");
+    }
 }
 </script>
 
