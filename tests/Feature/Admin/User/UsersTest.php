05b8d3ee7a28dd52172b314489cd23f842670a34
Russkazkin <ruslan@skazkin.su>
2020-10-01 11:07:11 +0300

    Add users json test

--- tests/Feature/Admin/User/UsersTest.php
+++ tests/Feature/Admin/User/UsersTest.php
@@ -48,4 +48,57 @@ test('super_admin_can_fetch_users', function () {
     $response->assertOk();
 });
 
+it('get_proper_users_json', function () {
+    /* @var \Tests\TestCase $this */
+
+    $superAdminUser = factory(User::class)->create();
+    $user = factory(User::class)->create();
+
+    Role::create(['name' => 'admin']);
+    Role::create(['name' => 'super-admin']);
+
+    $superAdminUser->assignRole('super-admin');
+
+    $this->actingAs($superAdminUser, 'api');
+
+    $response = $this->get('/api/users');
+
+    $response->assertJsonCount(2)->assertJson([
+        'data' => [
+            [
+                'data' => [
+                    'attributes' => [
+                        'avatar' => $superAdminUser->avatar,
+                        'name' => $superAdminUser->name,
+                        'email' => $superAdminUser->email,
+                    ],
+                    'type' => 'users',
+                    'user_id' => $superAdminUser->id,
+                ],
+                'links' => [
+                    'self' => url('/users/' . $superAdminUser->id),
+                ],
+            ],
+            [
+                'data' => [
+                    'attributes' => [
+                        'avatar' => $user->avatar,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ],
+                    'type' => 'users',
+                    'user_id' => $user->id,
+                ],
+                'links' => [
+                    'self' => url('/users/' . $user->id),
+                ],
+            ],
+        ],
+        'links' => [
+            'self' => url('/admin/users')
+        ]
+    ]);
+
+});
+
 
