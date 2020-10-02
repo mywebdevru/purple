33215f6f0771ab3be6b121cc3c75e79fe5134a4c
Russkazkin <ruslan@skazkin.su>
2020-10-02 10:01:48 +0300

    Add users_cant_delete_user test

--- tests/Feature/Admin/User/UserDeleteTest.php
+++ tests/Feature/Admin/User/UserDeleteTest.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\User;
+
 test('guests_cant_delete_user', function () {
     /* @var \Tests\TestCase $this */
 
     $response = $this->delete('/api/users/delete', [], ['Accept' => 'application/json']);
     $response->assertStatus(401);
+});
+
+test('users_cant_delete_user', function () {
+    /* @var \Tests\TestCase $this */
+
+    $this->actingAs(factory(User::class)->create(), 'api');
+    $response = $this->delete('/api/users/delete', []);
+    $response->assertForbidden();
 });
