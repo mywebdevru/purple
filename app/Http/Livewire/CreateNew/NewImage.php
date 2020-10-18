a0b196a5c944a857c8e0cb675466707cb807fb34
x3mart <45395171+x3mart@users.noreply.github.com>
2020-10-18 14:12:12 +0300

    Квота на загрузку файла увеличина до 20МБ. Добавленны эффекты при ресайзе фотографий

--- app/Http/Livewire/CreateNew/NewImage.php
+++ app/Http/Livewire/CreateNew/NewImage.php
@@ -25,14 +25,14 @@ class NewImage extends Component
     public function updatedPhoto()
     {
         $this->validate([
-            'photo' => 'image|max:2048', // 2MB Max
+            'photo' => 'image',
         ]);
     }
 
     public function saveImage()
     {
         $this->validate([
-            'photo' => 'image|max:2048', // 2MB Max
+            'photo' => 'image',
             'description' => 'string|nullable',
         ]);
         $this->newImage = $this->photo->store('photos');
@@ -40,8 +40,11 @@ class NewImage extends Component
         if ($img ->width() > 1024){
             $img ->resize(1024, null, function ($constraint) {
                 $constraint->aspectRatio();
-            });
-            $img ->save();
+            })
+            ->sharpen(3)
+            ->gamma(1.1)
+            ->contrast(5)
+            ->save();
         }
         $image = User::find(auth()->user()->id)->images()->create(['image' => $this->newImage, 'description' => $this->description]);
         $image->feed()->create(['authorable_id' => auth()->user()->id, 'authorable_type' => User::class]);
