c2c47837d1282846f7964ba7b55e094b6ecd7bbb
Russkazkin <ruslan@skazkin.su>
2020-10-19 10:07:36 +0300

    Add authUserFriends logic into vuex user module

--- resources/js/store/modules/user.js
+++ resources/js/store/modules/user.js
@@ -1,11 +1,16 @@
 const state = {
     authUser: false,
-    userStatus: null,
+    authUserStatus: null,
+    authUserFriends: [],
+    authUserFriendsStatus: null,
 };
 const getters = {
     authUser: state => {
         return state.authUser;
-    }
+    },
+    authUserFriends: state => {
+        return state.authUserFriends;
+    },
 };
 const actions = {
     async fetchAuthUser({commit}){
@@ -17,16 +22,33 @@ const actions = {
         } catch (error) {
             commit("setAuthUserStatus", "error");
         }
-    }
+    },
+
+    async fetchAuthUserFriends({commit}){
+        commit("setAuthUserFriendsStatus", "loading");
+        try {
+            // TODO Тест для этого роута
+            const friends = (await axios.get('/api/auth-user-friends')).data;
+            commit("setAuthUserFriends", friends);
+            commit("setAuthUserFriendsStatusStatus", "success");
+        } catch (error) {
+            commit("setAuthUserFriendsStatusStatus", "error");
+        }
+    },
 };
 const mutations = {
     setAuthUser(state, authUser) {
         state.authUser = authUser;
     },
+    setAuthUserFriends(state, friends) {
+        state.authUserFriends = friends;
+    },
     setAuthUserStatus(state, status) {
         state.authUserStatus = status;
     },
-
+    setAuthUserFriendsStatus(state, status) {
+        state.authUserFriendsStatus = status;
+    },
 };
 
 export default { state, getters, actions, mutations };
