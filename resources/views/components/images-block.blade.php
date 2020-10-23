9ef8c40f4ff6f9fb6a58ec43c681a89d1d7d4c9a
moskvicheveg <moskvicheveg@gmail.com>
2020-10-23 15:39:59 +0400

    added modal window for gallery

--- resources/views/components/images-block.blade.php
+++ resources/views/components/images-block.blade.php
@@ -3,11 +3,11 @@
         <h6 class="title">Фото ({{ $imagesCount() }})</h6>
     </div>
     <div class="ui-block-content">
-        <ul class="widget w-last-photo js-zoom-gallery">
+        <ul class="widget w-last-photo">
             @foreach ($images as $item)
                 @if ($loop->iteration < 9)
                     <li>
-                    <a href="{{ $imgSrc($item->image) }}">
+                    <a href="" data-toggle="modal" data-target="#open-photo-popup">
                             <img src="{{ $imgSrc($item->image) }}" alt="photo">
                         </a>
                     </li>
@@ -24,4 +24,37 @@
             @endif
         </ul>
     </div>
+</div>
+<div class="modal fade modal-has-swiper" id="open-photo-popup">
+	<div class="modal-dialog window-popup open-photo-popup">
+		<div class="modal-content">
+			<div class="modal-body-gallery">
+				<div class="open-photo-thumb">
+					<div class="swiper-container" data-slide="fade">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide">
+								<div class="photo-item">
+									<img src="https://static1.bocoup.com/assets/2015/11/05183211/font-face.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="open-photo-content">
+					<article class="hentry post">
+						<div class="post__author author vcard inline-items">
+                            <div class="author-date">
+                                <a class="h6 post__author-name fn" href="">Author / Photogrpher</a>
+                                <div class="post__date">
+                                    <time class="published" datetime="">
+
+                                    </time>
+                                </div>
+                            </div>
+					    </div>
+					</article>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
