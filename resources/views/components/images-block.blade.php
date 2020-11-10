f5a3c87ea2840c5597fb4151922a98fff939aedd
moskvicheveg <moskvicheveg@gmail.com>
2020-11-10 18:12:52 +0400

    new gallery

--- resources/views/components/images-block.blade.php
+++ resources/views/components/images-block.blade.php
@@ -1,14 +1,17 @@
-<div class="ui-block revealator-fade revealator-delay3 revealator-once">
+<div class="ui-block">
     <div class="ui-block-title">
         <h6 class="title">Фото ({{ $imagesCount() }})</h6>
     </div>
     <div class="ui-block-content">
-        <ul class="widget w-last-photo js-zoom-gallery">
+        <ul class="widget w-last-photo">
             @foreach ($images as $item)
                 @if ($loop->iteration < 9)
                     <li>
-                        <a href="#">
-                            <img src="{{ $imgSrc($item->image) }}" alt="photo">
+
+
+
+                        <a href="#" class="open-image-modal" data-path="{{ $imgSrc($item->image) }}">
+                                <img src="{{ $imgSrc($item->image) }}" alt="photo">
                         </a>
                     </li>
                 @else
@@ -26,20 +29,94 @@
     </div>
 </div>
 
-<script>
-    // function img() {
-    //     var dataSrc = document.getElementsByClassName("photo-src");
-    //     var modalImg = document.getElementsByClassName("modal-img");
-    //     console.log(modalImg);
-    //     for (let i = 0; i < dataSrc.length; i++) {
-    //         var currentSrc = dataSrc[i].getAttribute('data-src');
-    //         console.log(currentSrc);
-    //     }
-    //     for (let l = 0; l < modalImg.length; l++) {
-    //         modalImg[l].setAttribute('src', currentSrc);
-    //         console.log(modalImg[l])
-    //     }
-    // }
-    // img();
+<div class="portfolio" id="portfolio">
+            <div class="portfolio__col" data-cat="app">
+                <div class="work" data-modal="#modal_project_1">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
 
-</script>
+            <div class="portfolio__col" data-cat="website">
+                <div class="work" data-modal="#modal_project_2">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
+
+            <div class="portfolio__col" data-cat="interaction">
+                <div class="work">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
+
+            <div class="portfolio__col" data-cat="website">
+                <div class="work">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
+
+            <div class="portfolio__col" data-cat="interaction">
+                <div class="work">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
+
+            <div class="portfolio__col" data-cat="app">
+                <div class="work">
+                    <img class="work__image" src="https://placehold.it/370x300" alt="">
+                </div>
+            </div>
+        </div><!-- /.portfolio -->
+
+<div class="modal" id="modal_project_1">
+    <div class="modal__dialog">
+
+        <button class="modal__close" type="button" data-close>
+            <img src="{{ asset('img/modal-close.svg') }}" alt="Close">
+        </button>
+
+        <div class="modal-work">
+            <div class="modal-work__preview">
+                <div data-slider="slick">
+                    <div>
+                        <img class="modal-work__photo" src="https://placehold.it/790x780" alt="">
+                    </div>
+                    <div>
+                        <img class="modal-work__photo" src="https://placehold.it/790x780/333" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-work__content">
+
+                <div class="modal-work__header">
+                    <h3 class="modal-work__title">Project title</h3>
+                    <div class="modal-work__info">
+                        category <span class="modal-work__info-divider">|</span> 2018
+                    </div>
+                </div>
+
+                <div class="modal-work__client">
+                    <div class="modal-work__client-title">Client:</div>
+                    <div class="modal-work__client-company">Creative Agency</div>
+                </div>
+
+                <div class="modal-work__text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta, recusandae debitis iusto quos voluptatum at. Dolorum a, velit rerum dicta aut sapiente, optio accusantium? Sunt sed praesentium est minima.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dicta, recusandae debitis iusto quos voluptatum at. Dolorum a, velit rerum dicta aut sapiente, optio accusantium? Sunt sed praesentium est minima.</p>
+                </div>
+
+                <div class="modal-work__footer">
+                    <button class="modal-work__btn slickPrev" type="button">
+                        <img src="{{ asset('img/back.svg') }}" height="11" alt="">
+                        Previous
+                    </button>
+                    <button class="modal-work__btn slickNext" type="button">
+                        Next
+                        <img src="{{ asset('img/next.svg') }}" height="11" alt="">
+                    </button>
+                </div>
+
+            </div><!-- /.modal-work__content -->
+        </div><!--/.modal-work -->
+
+    </div><!-- /.modal__dialog -->
+</div><!-- /.modal -->
