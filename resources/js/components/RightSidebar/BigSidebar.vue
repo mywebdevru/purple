ff983e5f3e069d9161e35a1618a6561ad91495f8
Russkazkin <ruslan@skazkin.su>
2021-05-10 08:07:26 +0300

    FFRD1-65: Optimize RightSidebar components structure

---
+++ resources/js/components/RightSidebar/BigSidebar.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="fixed-sidebar-right sidebar--large" id="sidebar-right-1">
+
+    <div class="mCustomScrollbar" data-mcs-theme="dark">
+
+        <div class="ui-block-title ui-block-title-small">
+            <a href="#" class="title">Друзья</a>
+            <a href="#">Настройки</a>
+        </div>
+
+        <ul class="chat-users">
+            <!--                    loop-->
+            <li v-for="(friend, index) in friends" :key="index" class="inline-items js-chat-open">
+                <div class="author-thumb">
+                    <img alt="author" :src="friend.data.attributes.avatar" class="avatar">
+                    <span class="icon-status online"></span>
+                </div>
+
+                <div class="author-status">
+                    <a :href="friend.links.self" class="h6 author-name">{{ friend.data.attributes.full_name }}</a>
+                    <span class="status">ONLINE</span>
+                </div>
+                <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                    <ul class="more-icons">
+                        <li @click="$parent.startChat(friend.data.user_id)">
+                            <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                        </li>
+
+                        <li>
+                            <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                        </li>
+
+                        <li>
+                            <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <!-- /loop -->
+        </ul>
+
+    </div>
+
+    <div class="search-friend inline-items">
+        <form class="form-group" >
+            <input class="form-control" placeholder="Поиск друзей..." value="" type="text">
+        </form>
+
+        <a href="#" class="settings">
+            <svg class="olymp-settings-icon"><use href="/svg-icons/sprites/icons.svg#olymp-settings-icon"></use></svg>
+        </a>
+
+        <a href="#" class="js-sidebar-open" @click.prevent="$parent.sidebarToggle">
+            <svg class="olymp-close-icon"><use href="/svg-icons/sprites/icons.svg#olymp-close-icon"></use></svg>
+        </a>
+    </div>
+
+    <a href="#" class="olympus-chat inline-items js-chat-open">
+
+        <h6 class="olympus-chat-title">Чат</h6>
+        <svg class="olymp-chat---messages-icon"><use href="/svg-icons/sprites/icons.svg#olymp-chat---messages-icon"></use></svg>
+    </a>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "BigSidebar",
+    props: {
+        friends: {
+            type: Array,
+            required: false,
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/RightSidebar/MobileSidebar.vue
@@ -0,0 +1,220 @@
+<template>
+    <div class="fixed-sidebar right fixed-sidebar-responsive" id="sidebar-right-responsive">
+
+        <div class="fixed-sidebar-right sidebar--small">
+            <a href="#" class="js-sidebar-open">
+                <svg class="olymp-menu-icon"><use href="/svg-icons/sprites/icons.svg#olymp-menu-icon"></use></svg>
+                <svg class="olymp-close-icon"><use href="/svg-icons/sprites/icons.svg#olymp-close-icon"></use></svg>
+            </a>
+        </div>
+
+        <div class="fixed-sidebar-right sidebar--large">
+            <div class="mCustomScrollbar" data-mcs-theme="dark">
+
+                <div class="ui-block-title ui-block-title-small">
+                    <a href="#" class="title">Друзья</a>
+                    <a href="#">Настройки</a>
+                </div>
+
+                <ul class="chat-users">
+                    <li class="inline-items js-chat-open">
+
+                        <div class="author-thumb">
+                            <img alt="author" src="/img/spiegel.jpg" class="avatar">
+                            <span class="icon-status online"></span>
+                        </div>
+
+                        <div class="author-status">
+                            <a href="#" class="h6 author-name">spiegel</a>
+                            <span class="status">ONLINE</span>
+                        </div>
+
+                        <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                            <ul class="more-icons">
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                                </li>
+                            </ul>
+
+                        </div>
+
+                    </li>
+                    <li class="inline-items js-chat-open">
+
+                        <div class="author-thumb">
+                            <img alt="author" src="/img/spiegel.jpg" class="avatar">
+                            <span class="icon-status online"></span>
+                        </div>
+
+                        <div class="author-status">
+                            <a href="#" class="h6 author-name">spiegel</a>
+                            <span class="status">AT WORK!</span>
+                        </div>
+
+                        <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                            <ul class="more-icons">
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                                </li>
+                            </ul>
+
+                        </div>
+
+                    </li>
+
+                    <li class="inline-items js-chat-open">
+
+
+                        <div class="author-thumb">
+                            <img alt="author" src="/img/spiegel.jpg" class="avatar">
+                            <span class="icon-status online"></span>
+                        </div>
+
+                        <div class="author-status">
+                            <a href="#" class="h6 author-name">spiegel</a>
+                            <span class="status">ONLINE</span>
+                        </div>
+
+                        <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                            <ul class="more-icons">
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                                </li>
+                            </ul>
+
+                        </div>
+
+
+                    </li>
+
+                    <li class="inline-items js-chat-open">
+
+
+                        <div class="author-thumb">
+                            <img alt="author" src="/img/spiegel.jpg" class="avatar">
+                            <span class="icon-status away"></span>
+                        </div>
+
+                        <div class="author-status">
+                            <a href="#" class="h6 author-name">spiegel</a>
+                            <span class="status">AWAY</span>
+                        </div>
+
+                        <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                            <ul class="more-icons">
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                                </li>
+                            </ul>
+
+                        </div>
+
+
+                    </li>
+
+                    <li class="inline-items js-chat-open">
+
+
+                        <div class="author-thumb">
+                            <img alt="author" src="/img/spiegel.jpg" class="avatar">
+                            <span class="icon-status disconected"></span>
+                        </div>
+
+                        <div class="author-status">
+                            <a href="#" class="h6 author-name">spiegel</a>
+                            <span class="status">OFFLINE</span>
+                        </div>
+
+                        <div class="more"><svg class="olymp-three-dots-icon"><use href="/svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+
+                            <ul class="more-icons">
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="START CONVERSATION" class="olymp-comments-post-icon"><use href="/svg-icons/sprites/icons.svg#olymp-comments-post-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="ADD TO CONVERSATION" class="olymp-add-to-conversation-icon"><use href="/svg-icons/sprites/icons.svg#olymp-add-to-conversation-icon"></use></svg>
+                                </li>
+
+                                <li>
+                                    <svg data-toggle="tooltip" data-placement="top" data-original-title="BLOCK FROM CHAT" class="olymp-block-from-chat-icon"><use href="/svg-icons/sprites/icons.svg#olymp-block-from-chat-icon"></use></svg>
+                                </li>
+                            </ul>
+
+                        </div>
+
+
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="search-friend inline-items">
+                <form class="form-group" >
+                    <input class="form-control" placeholder="Поиск друзей..." value="" type="text">
+                </form>
+
+                <a href="" class="settings">
+                    <svg class="olymp-settings-icon"><use href="/svg-icons/sprites/icons.svg#olymp-settings-icon"></use></svg>
+                </a>
+
+                <a href="#" class="js-sidebar-open">
+                    <svg class="olymp-close-icon"><use href="/svg-icons/sprites/icons.svg#olymp-close-icon"></use></svg>
+                </a>
+            </div>
+
+            <a href="#" class="olympus-chat inline-items js-chat-open">
+
+                <h6 class="olympus-chat-title">Чат</h6>
+                <svg class="olymp-chat---messages-icon"><use href="/svg-icons/sprites/icons.svg#olymp-chat---messages-icon"></use></svg>
+            </a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MobileSidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
