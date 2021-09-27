f98821c68c9ab1bec78491c1ba276c285fc55829
Russkazkin <ruslan@skazkin.su>
2021-09-27 15:05:01 +0300

    Upgrade Comment seeder and factory

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -13,14 +13,15 @@ class ImageSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws \Exception
      */
-    public function run()
+    public function run(): void
     {
-        factory(Image::class, 300)->create()->each(function (Image $image){
-            $comments = factory(Comment::class, rand(1, 10))->make();
+        Image::factory()->count(300)->create()->each(function (Image $image){
+            $comments = Comment::factory()->count(random_int(1, 10))->make();
             $comments->each(function ($comment) {
-                $comment->authorable_id = rand(1, User::count());
-                $comment->authorable_type = 'App\Models\User';
+                $comment->authorable_id = random_int(1, User::count());
+                $comment->authorable_type = User::class;
             });
             $image->comments()->saveMany($comments);
         });
