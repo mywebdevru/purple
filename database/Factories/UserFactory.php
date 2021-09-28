18cfcbad56797efcf4231f29cae1c280e901d777
Russkazkin <ruslan@skazkin.su>
2021-09-28 07:21:33 +0300

    Upgrade User factory

--- database/Factories/UserFactory.php
+++ database/Factories/UserFactory.php
@@ -1,36 +1,44 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use JetBrains\PhpStorm\ArrayShape;
 use App\Models\User;
-use Faker\Generator as Faker;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->firstName,
-        'surname' => $faker->lastName,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$rf7MHVdVCW4lLau/gDCQBOyLayW9nlQlk6nnVssx63LUcvgv1QgEe', // password 123
-        'remember_token' => Str::random(10),
-        'city' => $faker->city,
-        'country' => $faker->country,
-        'gender' =>  Arr::random(['Мужчина', 'Женщина']),
-        'birth_date' =>$faker->date(),
-        'wallpaper' => 'https://picsum.photos/seed/'.rand(1, 1000).'/1200/400',
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws \Exception
+     */
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->firstName,
+            'surname' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$rf7MHVdVCW4lLau/gDCQBOyLayW9nlQlk6nnVssx63LUcvgv1QgEe', // password 123
+            'remember_token' => Str::random(10),
+            'city' => $this->faker->city,
+            'country' => $this->faker->country,
+            'gender' =>  Arr::random(['Мужчина', 'Женщина']),
+            'birth_date' =>$this->faker->date(),
+            'wallpaper' => 'https://picsum.photos/seed/' . random_int(1, 1000) . '/1200/400',
+        ];
+    }
+}
